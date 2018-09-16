5781f4aa4aae634cea230c2539f62a1bb781b55a
Jacopo Jannone <jacopo-j@users.noreply.github.com>
2018-09-16 14:01:23 +0200

    cerca soluzioni

--- trenitalia.py
+++ trenitalia.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+Author: Jacopo Jannone
+Repository: https://github.com/jacopo-j/TrenitaliaAPI
+License: MIT
+Copyright: (c) 2018 Jacopo Jannone
+
+Questo commento non deve MAI essere rimosso da questo file e deve
+essere riportato integralmente nell'intestazione di qualunque programma
+che faccia uso del codice di seguito o di parte di esso.
+
+This comment may NOT be removed from this file and it must be present
+on the header of any program using this code or any part of it.
+
+------------------------------------------------------------------------
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+
 import requests
 import uuid
 import json
@@ -29,6 +65,9 @@ class MultipleTrainsFound(Exception):
 class TrainCancelledException(Exception):
     pass
 
+class NoSolutionsFound(Exception):
+    pass
+
 
 class TrenitaliaBackend():
     VERSION = '5.0.1.0004'
@@ -37,6 +76,7 @@ class TrenitaliaBackend():
     BACKEND_PATH = 'Trenitalia50/apps/services/api/Trenitalia/android/'
     INIT_URL = f'{HOST}{BACKEND_PATH}init'
     QUERY_URL = f'{HOST}{BACKEND_PATH}query'
+    NIL = {"nil": True}
 
     def __init__(self):
         self._session = requests.session()
@@ -145,6 +185,127 @@ class TrenitaliaBackend():
                            "railway": int(station["railwaycode"])})
         return data
 
+    def search_solution(self, origin, destination, dep_date, arr_date=None,
+                        adults=1, children=0, train_type="All",
+                        max_changes=99, limit=10):
+        cur_index = 0
+        if dep_date is None:
+            depdrange = None
+        else:
+            depdrange = {"Start": self._build_date(dep_date), "End": None}
+        if arr_date is None:
+            arrdrange = None
+        else:
+            arrdrange = {"Start": self._build_date(arr_date), "End": None}
+        while cur_index < limit:
+            p = [{'AppVersion': self.VERSION_SHORT,
+                  'Credentials': None,
+                  'CredentialsAlias': None,
+                  'DeviceId': self._device_id,
+                  'Language': 'IT',
+                  'PlantId': 'android',
+                  'PointOfSaleId': 3,
+                  'UnitOfWork': 0},
+                  {'SearchTravelsRequest':
+                        {'Body': {'PagingCriteria': {"StartIndex": cur_index,
+                                                     "EndIndex": cur_index,
+                                                     "SortDirection": None},
+                                  "OriginStationId": origin,
+                                  "DestinationStationId": destination,
+                                  "DepartureDateTimeRange": depdrange,
+                                  "ArrivalDateTimeRange": arrdrange,
+                                  "ReturnDateTimeRange": None,
+                                  "ArrivalReturnDateTimeRange": None,
+                                  "IsReturn": False,
+                                  "Passengers": {"PassengerQuantity": [
+                                    {"Type": "Adult", "Quantity": adults},
+                                    {"Type": "Child", "Quantity": children}]},
+                                  "FidelityCardCode": None,
+                                  "PostSaleCriteria": None,
+                                  "TrainType": train_type,
+                                  "MaxNumberOfChanges": str(max_changes)}}}]
+            for i in range(2):
+                r = self._session.post(self.QUERY_URL,
+                                       data={"adapter": "SearchAndBuyAdapter",
+                                             "procedure": "SearchTravels",
+                                             "parameters": json.dumps(p)})
+                result = json.loads(self._cleanup(r.text))
+                if r.status_code == 200:
+                    break
+                if i > 0:
+                    raise AuthenticationError("Authentication attempt failed "
+                                              "after getting non 200 status code")
+                self._authenticate(result)
+            if result["statusCode"] == 500:
+                if result["statusReason"].startswith("Nessuna soluzione"):
+                    raise NoSolutionsFound()
+                if result["statusReason"] == ("Errore restituito dal sistema "
+                                              "centrale"):
+                    return
+            if (result["statusCode"] != 200):
+                raise Non200StatusCode("Response statusCode {}: {}".format(
+                                       result["statusCode"],
+                                       result["statusReason"]))
+            solution = (result["Envelope"]["Body"]["SearchTravelsResponse"]
+                        ["Body"]["PageResult"]["TravelSolution"])
+            output = {"changes": int(solution["Changes"]),
+                           "destination":
+                              {"name": solution["DestinationStation"]["Name"],
+                               "id": solution["DestinationStation"]["Id"]},
+                            "origin":
+                              {"name": solution["OriginStation"]["Name"],
+                               "id": solution["OriginStation"]["Id"]},
+                            "duration": self._parse_time(
+                                solution["TotalJourneyTime"]),
+                            "arr_date": self._parse_date(
+                                solution["ArrivalDateTime"]),
+                            "dep_date": self._parse_date(
+                                solution["DepartureDateTime"]),
+                            "saleable": solution["IsSaleable"],
+                            "solution_id": solution["SolutionId"],
+                            "vehicles": [],
+                            "min_points": Decimal(solution["MinLoyaltyPoints"])
+                                if "MinLoyaltyPoints" in solution and
+                                solution["MinLoyaltyPoints"] != self.NIL
+                                else None,
+                            "min_price": Decimal(solution["MinPrice"])
+                                if solution["MinPrice"] != self.NIL else None}
+            if isinstance(solution["Nodes"]["SolutionNode"], list):
+                for v in solution["Nodes"]["SolutionNode"]:
+                    vh_data = {"dep_date": self._parse_date(
+                                            v["DepartureDateTime"]),
+                               "arr_date": self._parse_date(
+                                            v["ArrivalDateTime"]),
+                               "category": (v["Train"]["CategoryCode"],
+                                            v["Train"]["CategoryName"]),
+                               "number": v["Train"]["Number"],
+                               "arr_station":
+                                    {"name": v["ArrivalStation"]["Name"],
+                                     "id": v["ArrivalStation"]["Id"]},
+                               "dep_station":
+                                    {"name":v["DepartureStation"]["Name"],
+                                     "id": v["DepartureStation"]["Id"]},
+                               "id": v["Id"],
+                               "duration": self._parse_time(
+                                            v["JourneyDuration"])}
+                    output["vehicles"].append(vh_data)
+            else:
+                v = solution["Nodes"]["SolutionNode"]
+                vh_data = {"dep_date":self._parse_date(v["DepartureDateTime"]),
+                           "arr_date": self._parse_date(v["ArrivalDateTime"]),
+                           "category": (v["Train"]["CategoryCode"],
+                                        v["Train"]["CategoryName"]),
+                           "number": v["Train"]["Number"],
+                           "arr_station": {"name": v["ArrivalStation"]["Name"],
+                                           "id": v["ArrivalStation"]["Id"]},
+                           "dep_station": {"name":v["DepartureStation"]["Name"],
+                                           "id": v["DepartureStation"]["Id"]},
+                           "id": v["Id"],
+                           "duration": self._parse_time(v["JourneyDuration"])}
+                output["vehicles"].append(vh_data)
+            yield output
+            cur_index += 1
+
     def train_info(self, number, dep_st=None, arr_st=None, dep_date=None):
         p = [{'AppVersion': self.VERSION_SHORT,
               'Credentials': None,
@@ -245,6 +406,5 @@ class TrenitaliaBackend():
 
 
 tb = TrenitaliaBackend()
-pprint(tb.search_station(""))
-
+pprint(list(tb.search_solution("830008224", "830012055", datetime(2018, 9, 15, 16, 0, 0), limit=10)))
 
